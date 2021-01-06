d35d8e88c9a8944cac11c8a1df5ef58134dcd3ec
Erick Díaz Saborio <ewdiazsa@gmail.com>
2021-01-05 18:41:19 -0600

    Se agregaron leyendas a la grafica del ejercicio 3

--- laboratorio1/tarea3.m
+++ laboratorio1/tarea3.m
@@ -7,6 +7,10 @@ subplot(2,1,1);
 stem(t,real(x1));
 hold on;
 plot(t, real(x1), 'Color', 'red', 'LineStyle','--');
+xlabel('t');
+ylabel('x_1(t)');
+title('x_1(n)=Re\{e^{j 14\pi/17 n}\}');
+legend('discreta', 'continua')
 hold off;
 
 % Grafica 2
@@ -14,4 +18,8 @@ subplot(2,1,2)
 stem(t,x2);
 hold on;
 plot(t, x2, 'Color', 'red', 'LineStyle','--');
+xlabel('t');
+ylabel('x_2(t)');
+title('x_2(n) = cos(1/5 n)');
+legend('discreta', 'continua');
 hold off;

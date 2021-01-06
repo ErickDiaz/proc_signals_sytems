7761e9102f3d9bfc6a5337fc7685168a7b7ea0be
Erick Díaz Saborio <ewdiazsa@gmail.com>
2021-01-05 19:28:12 -0600

    se actulizo titulos en grafica 2

--- laboratorio1/tarea2.m
+++ laboratorio1/tarea2.m
@@ -4,24 +4,25 @@ x_imag = imag(exp(j*(pi/2)*t));
 x_abs = abs(exp(j*(pi/2)*t));
 x_angle = angle(exp(j*(pi/2)*t));
 
-subplot(1,2,1);
+
 % Grafica 1
+subplot(2,1,1);
 plot(t, x_real);
 hold on;
 plot(t, x_imag);
 hold off;
 xlabel('t');
 ylabel('x(t)');
-title('Parte Real e imaginaria');
-legend('real(exp(j*(pi/2)*t))', 'imag(exp(j*(pi/2)*t));')
+title('e^{j*(\pi/2)*t}');
+legend('Parte real', 'Parte imaginaria')
 
 % Grafica 2
-subplot(1,2,2);
+subplot(2,1,2);
 plot(t, x_abs);
 hold on;
 plot(t, x_angle);
 hold off;
 xlabel('t');
 ylabel('x(t)');
-title('Valor absoluto y fase de la señal');
-legend('abs(exp(j*(pi/2)*t))', 'angle(exp(j*(pi/2)*t));')
+title('e^{j*(\pi/2)*t}');
+legend('Valor absoluto', 'Fase de la señal');

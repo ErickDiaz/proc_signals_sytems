9dbc318855477446c3a048ecee47d32266cd32ab
Erick Díaz Saborio <ewdiazsa@gmail.com>
2021-02-10 04:52:03 -0600

    Tarea 4 media y primir coeficiente DFT

---
+++ actividad_2/tarea4.m
@@ -0,0 +1,24 @@
+clear;
+
+T=6;
+
+inct=0.01;
+t=0:inct:T;
+fs=1/inct;
+N=T/inct; % Definimos el número de muestras
+x = 0.*(t<2.9)+2.*((t>=2.9)&(t<=3))+0.*(t>3);
+
+media = mean(x);
+
+% Calculamos FFT
+xf = fft(x) ;
+
+% Segun la ecuacion de analisis DFT X[m] para m=0 es
+X_0= sum(x);
+
+% Dividimos entre fs para ajustar la amplitud de la FFT y que se corresponda con la amplitud real de la FT
+xf = xf/N;
+
+X_0=X_0/N
+xf(1)
+media
